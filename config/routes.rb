02c96861d7afebab72664c59c40ffeaadc1f9d50
kobasa-tech <warped.trump@gmail.com>
2021-07-04 17:02:12 +0900

    createアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
   # ルートパス(localhost:3000へのパス)を記述
-  resources :tweets, only: [:index, :new]
+  resources :tweets, only: [:index, :new, :create]
   # resourcesメソッドでルーティングを記述
   # resources :コントローラー名, onlyオプション: :必要なアクション名
 end
