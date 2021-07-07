5c46d371553eb78fc7d95ab27c8856ea5b789b89
kobasa-tech <warped.trump@gmail.com>
2021-07-07 23:23:50 +0900

    投稿者のマイページにアクセスできるようにusersコントローラーを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname #ログインしているユーザーのニックネームカラムを取得してインスタンス変数に代入
-    @tweets = current_user.tweets #ログインしているユーザーが投稿したツイート全てを取得してインスタンス変数に代入
+    user = User.find(params[:id]) # あるユーザーの詳細ページに遷移する際のパラメーターからidを取得
+    @nickname = user.nickname #取得したidのユーザーのニックネームカラムを取得してインスタンス変数に代入
+    @tweets = user.tweets #取得したidのユーザーが投稿したツイート全てを取得してインスタンス変数に代入
+    # 取得したパラメーター Parameters: {"id"=>"2"}
   end
 end
