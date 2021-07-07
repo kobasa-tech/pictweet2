ebfac65f8d327908bc2bd0ac39b02edb352c3eb0
kobasa-tech <warped.trump@gmail.com>
2021-07-07 20:55:47 +0900

    アソシエーションの設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,8 @@
 class Tweet < ApplicationRecord
   validates :text, presence: true
   # textカラムが空では投稿できない、という制約。validates :カラム名(シンボル), presence: true(空では登録できない)
+  belongs_to :user
+  # アソシエーションの設定。ツイート対ユーザーは他対1の関係。
+  # アソシエーションでbelongs_toを指定した場合、相手のモデルのid(user_id)が
+  # 「空ではないか」というバリデーション validates :user_id, presence: true がデフォルトでかかるようになっている。
 end
