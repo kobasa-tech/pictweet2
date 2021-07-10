54ecb3b839ab68f40a829c9ca455d6037886adba
kobasa-tech <warped.trump@gmail.com>
2021-07-10 19:01:53 +0900

    tweets/showビューにコメント一覧を表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,11 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
+    # Parameters {"authenticity_token"=>"KW6xJ/略==", "comment"=>{"text"=>"params"}, "commit"=>"SEND", "controller"=>"comments",
+    #  "action"=>"create", "tweet_id"=>"7"} permitted: false>
     params.require(:comment).permit(:text).merge(user_id: current_user.id,tweet_id: params[:tweet_id])
     # パラメーターとしてテキストに加え、現在のユーザーidとコメントを書き込んでいるツイートのid情報を取得する。
     # tweet_idキーのバリューであるparams[:tweet_id]はネストしたルーティングで取得できるtweet_idである。
+    # Parameters {"text"=>"params", "user_id"=>4, "tweet_id"=>"7"} permitted: true>
   end
 end
