1a482b836c26dbabe697f567075eda5b419221c3
kobasa-tech <warped.trump@gmail.com>
2021-07-10 16:47:21 +0900

    commentsコントローラーへのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   # ルートパス(localhost:3000へのパス)を記述
-  resources :tweets
+  resources :tweets do
+    resources :comments, only: :create
+    # ルーティングをネストすることでtweetのidをcommentsコントローラーに送れる
+    # ralis routesのURI /tweets/:tweet_id/comments(.:format)
+  end
   # resourcesメソッドでルーティングを記述
   # resources :コントローラー名, onlyオプション: :必要なアクション名(シンボル)
   resources :users, only: :show
