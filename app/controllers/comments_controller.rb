15569c26587b26ee11d64a065b204f6f9df8adff
kobasa-tech <warped.trump@gmail.com>
2021-07-10 18:04:09 +0900

    ツイートのshowビューにコメント投稿フォームを作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,13 @@ class CommentsController < ApplicationController
     comment = Comment.create(comment_params)
     # comment_paramsメソッドの内容をCommentテーブルに保存し、変数commentに代入。
     redirect_to "/tweets/#{comment.tweet.id}"
-    # リダイレクト先はコメントしたツイートのshowアクション。保存したコメントに結びつくツイートのidをパスに入れている。
+    # リダイレクト先はコメントしたツイートのshowアクション。保存したコメントに結びつくツイートのidをルートパスに入れている。
   end
 
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id,tweet_id: params[:tweet_id])
     # パラメーターとしてテキストに加え、現在のユーザーidとコメントを書き込んでいるツイートのid情報を取得する。
+    # tweet_idキーのバリューであるparams[:tweet_id]はネストしたルーティングで取得できるtweet_idである。
   end
 end
